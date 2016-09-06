ddcc9be749b085921d6e79938908c9ac344e2883
Andy Dobbels <andy@mailosaur.com>
2016-09-07 00:24:14 +0100

    Improve error message when a request fails.

--- src/Client.php
+++ src/Client.php
@@ -204,7 +204,7 @@ class Client
         $requestState = curl_getinfo($curl);
 
         if ($requestState['http_code'] != 200 && $requestState['http_code'] != 204) {
-            throw new Exception('Bad request. Check your credentials.');
+            throw new Exception("Error making request to " . $this->apiUrl . $path . " Http status code: " . $requestState['http_code']);
         }
 
         return $response;
